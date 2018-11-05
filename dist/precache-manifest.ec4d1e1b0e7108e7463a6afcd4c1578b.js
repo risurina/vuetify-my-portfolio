c69f67e819f1f255500f368f159a0842c397daf9
Ronnie Isurina <ronnie.isurina@gmail.com>
2018-11-06 00:58:43 +0800

    Build app.

---
+++ dist/precache-manifest.ec4d1e1b0e7108e7463a6afcd4c1578b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "62782e9335345e7b0c28",
+    "url": "/js/chunk-vendors.1e5d2d04.js"
+  },
+  {
+    "revision": "0f53db18cad416803b7a",
+    "url": "/js/app.6131b135.js"
+  },
+  {
+    "revision": "5a1d2f14ca084b9687f9",
+    "url": "/js/about.3d29723c.js"
+  },
+  {
+    "revision": "da5a9caca13ba627113c873c7866ddbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "62782e9335345e7b0c28",
+    "url": "/css/chunk-vendors.13951f06.css"
+  },
+  {
+    "revision": "0f53db18cad416803b7a",
+    "url": "/css/app.919b9827.css"
+  }
+];
